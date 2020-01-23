524aa88298e2768d0f043a5352f62d1b9bbda805
Dila91 <59875110+Dila91@users.noreply.github.com>
2020-01-23 15:34:47 +0100

    Initial commit

---
+++ Guessing Game/Controllers/GuessingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Guessing_Game.ViewModels;
+using Microsoft.AspNetCore.Http;
+
+namespace Guessing_Game.Controllers
+{
+    public class GuessingController : Controller
+    {
+        //GET - Create a random number every time we refresh the page
+        public ActionResult Index()
+        {
+            //Generate the number ans store it in a session
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            HttpContext.Session.SetInt32("Test Int", number);
+            HttpContext.Session.SetInt32("Attempts", 0);
+
+            GuessingViewModel m = new GuessingViewModel();
+            return View(m);
+        }
+
+        // POST: Check our guess
+        [HttpPost]
+        public ActionResult Index(GuessingViewModel m)
+        {
+            int? test = HttpContext.Session.GetInt32("Attempts");
+            test++;
+            HttpContext.Session.SetInt32("Attempts", (int)test);
+            m.CheckTheGuess(m.Guessed, HttpContext.Session.GetInt32("Test Int"), HttpContext.Session.GetInt32("Attempts"));
+            return View(m);
+        }
+    }
+}
---
+++ Guessing Game/ViewModels/GuessingViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Guessing_Game.ViewModels
+{
+    public class GuessingViewModel
+    {
+        public GuessingViewModel()
+        {
+            Message = "";
+            MaxAllowed = 6;
+        }
+        public int Guessed { get; set; }
+        public string Message { get; set; }
+        public int MaxAllowed { get; set; }
+        public int Attempts { get; set; }
+        public void CheckTheGuess(int guess, int? generated, int? attempts)
+        {
+            Attempts = (int)attempts;
+
+            if (attempts <= MaxAllowed)
+            {
+             if (guess < generated)
+                {
+                    Message = "Go higher!";
+                }
+                else if (guess > generated)
+                {
+                    Message = "Go lower!";
+                }
+                else
+                {
+                    Message = "You guessed correctly! Congratulations! Reload the page to reset the values!";
+                }
+            }
+            else
+            {
+                Message = "Too many attempts!";
+            }
+        }
+    }
+}
